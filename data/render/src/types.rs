1fd96be17ce167e41ae054de03510f157273d6f4
Matthias Endler <matthias-endler@gmx.net>
2020-06-05 11:22:18 +0200

    Replace custom trait impls with derives

--- data/render/src/types.rs
+++ data/render/src/types.rs
@@ -1,9 +1,8 @@
 use askama::Template;
 use std::cmp::Ordering;
 use std::collections::{BTreeMap, HashSet};
-use std::fmt;
 
-#[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Eq, Hash)]
+#[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Eq, Hash, Ord, PartialOrd)]
 pub enum Type {
     #[serde(alias = "language")]
     Language,
@@ -11,44 +10,12 @@ pub enum Type {
     Other,
 }
 
-impl fmt::Display for Type {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let s: &str = match self {
-            Self::Language => "language",
-            Self::Other => "other",
-        };
-        write!(f, "{}", s)
-    }
-}
-
-#[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Eq, Hash)]
+#[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Eq, Hash, Ord, PartialOrd)]
 pub struct Tag {
     pub name: String,
     pub tag: String,
     #[serde(alias = "type")]
     pub tag_type: Type,
-}
-
-impl fmt::Display for Tag {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(
-            f,
-            "{} tag: {}, type: {}",
-            self.name, self.tag, self.tag_type
-        )
-    }
-}
-
-impl Ord for Tag {
-    fn cmp(&self, other: &Tag) -> Ordering {
-        self.tag.to_lowercase().cmp(&other.tag.to_lowercase())
-    }
-}
-
-impl PartialOrd for Tag {
-    fn partial_cmp(&self, other: &Tag) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
 }
 
 // The tags from tags.yml Note that this is a `Vector<Tag>` and not a
@@ -57,22 +24,15 @@ pub type Tags = Vec<Tag>;
 
 pub type EntryTags = HashSet<String>;
 
-#[derive(Clone, Debug, Serialize, Deserialize, Eq)]
+#[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Eq)]
 pub struct Entry {
     pub name: String,
     pub url: String,
     pub description: String,
-    // TODO #[validate(length(min = 1))]
     pub tags: EntryTags,
     pub proprietary: Option<bool>,
     pub deprecated: Option<bool>,
     pub wrapper: Option<bool>,
-}
-
-impl PartialEq for Entry {
-    fn eq(&self, other: &Entry) -> bool {
-        self.name.to_lowercase() == other.name.to_lowercase()
-    }
 }
 
 impl PartialOrd for Entry {
